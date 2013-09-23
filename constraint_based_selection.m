8027753566a65e42562b6cdec2ab559843aefc85
Thiago <covoes@gmail.com>
2013-09-22 21:38:32 -0300

    Primeiras modificações relacionadas a estruturação do algoritmo

---
+++ constraint_based_selection.m
@@ -0,0 +1,14 @@
+function [feasiblePool infeasiblePool] = constraint_based_selection(P, Pmut, constraints, feasiblePool, infeasiblePool, data)
+%CONSTRAINT_BASED_SELECTION Performs (mu+lambda) selection filtering solutions that violates constraints
+%
+%Assumes that the intent is to minimize the fitness function
+	global DEBUG;
+	fullPop = [ P'; Pmut' ]; 
+	fitness = [fullPop(:).fitness];
+	[idxVio cost] = check_constraints(fullPop, constraints, data);
+	infeasiblePool = [infeasiblePool'; fullPop(idxVio)];
+	fullPop(idxVio) = [];
+	[bestFit idxSorted] = sort(fitness,'ascend');
+	P = fullPop(idxSorted(1:sizePopulation));
+	feasiblePool = [feasiblePool'; P'];
+end
---
+++ fitness_based_selection.m
@@ -0,0 +1,14 @@
+function [feasiblePool infeasiblePool] = fitness_based_selection(P, Pmut, constraints, feasiblePool, infeasiblePool)
+%FITNESS_BASED_SELECTION Performs (mu+lambda) selection filtering solutions that violates constraints
+%
+%Assumes that the intent is to minimize the fitness function
+	global DEBUG;
+	fullPop = [ P'; Pmut' ]; 
+	fitness = [fullPop(:).fitness];
+	idxVio = check_constraints(fullPop, constraints, data);
+	infeasiblePool = [infeasiblePool'; fullPop(idxVio)];
+	fullPop(idxVio) = [];
+	[bestFit idxSorted] = sort(fitness,'ascend');
+	P = fullPop(idxSorted(1:sizePopulation));
+	feasiblePool = [feasiblePool'; P'];
+end
