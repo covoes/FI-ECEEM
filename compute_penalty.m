6678b4e0db31992c61316f288f6e2f2359a1dc14
Thiago <covoes@gmail.com>
2014-01-16 23:41:52 -0200

    Bug corrigido na avaliação das restrições
    Antes estava comparando os índices dos clusters ao invés das classes

--- compute_penalty.m
+++ compute_penalty.m
@@ -1,8 +1,9 @@
 function [idxVio totPenalty] = compute_penalty(P, constraints, data)
-%COMPUTE_PENALTY Given a population, compute a penalty score for each individual. Return in idxVio 
+%COMPUTE_PENALTY Given a population, compute a penalty score for each individual and return in 
+%    idxVio a boolean vector of individuals with constraint violations
 
 idxVio = logical(zeros([1 length(p)]))
-totPenalty = logical(zeros([1 length(p)]))
+totPenalty = zeros([1 length(p)])
 
 for i=1:length(P)
 	for c=1:size(constraints,1)
@@ -13,18 +14,17 @@ for i=1:length(P)
 		%pk2 holds the analogue for idx2 [for k2]
 		[pk1,k1] = max(gauss(idx1,:))
 		[pk2,k2] = max(gauss(idx2,:))
+		classK1 = P(i).classOf(k1)
+		classK2 = P(i).classOf(k2)
 		penalty = 0
-		if constraints(c,3) == 1 && k1 != k2
+		if constraints(c,3) == 1 && classK1 != classK2
 			%ML constraint being violated
 			penalty = (1-gauss(idx2, k1)) + (1-gauss(idx1, k2))
-		else if constraints(c,3) == -1 && k1 == k2
+		else if constraints(c,3) == -1 && classK1 == classK2
 			%CL constraint being violated
       penalty = gauss(idx1, k1) +  gauss(idx2, k1)
 		end
     totPenalty(i) = totPenalty(i) + penalty
-	end
-	if totPenalty(i) > 0
-		idxVio(i) = true
 	end
 end
 idxVio(totPenalty>0) = true
