c03c88b1167162c8f0b189dc271dc2503725b6c3
Thiago Covões <covoes@gmail.com>
2014-05-15 09:57:40 -0300

    BUG: Soluções infeasible podem ter cluster sem rotulados
    Adicionado check para garantir que não troque a classe de um determinado
    cluster se ele é o único da classe atual

--- fix_class_unlabeled_clusters.m
+++ fix_class_unlabeled_clusters.m
@@ -6,15 +6,19 @@ function indiv = fix_class_unlabeled_clusters(indiv, gmmObj, sharedData)
 		return
 	end
 
-
+	oldIndiv = indiv;
 	labeled = sharedData.chunklets > 0;
+	nClustersPerClass = hist(indiv.classOfCluster, unique(indiv.classOfCluster));
 	for k=1:indiv.nClusters
 		objsInClusters = gmmObj.clusterLabels == k;
-		if ~any(sharedData.chunklets(objsInClusters))
+		if ~any(sharedData.chunklets(objsInClusters)) && ...
+				nClustersPerClass(indiv.classOfCluster(k)) > 1
 			indiv.classOfCluster(k) = closestClass(indiv.mean(k,:), ...
 				                     sharedData.data(labeled,:), sharedData.chunklets(labeled));
 		end
 	end
+	assert(isequal(sort(unique(indiv.classOfCluster(:))), ...
+		             sort(unique(oldIndiv.classOfCluster(:)))))
 end
 
 
@@ -28,6 +32,7 @@ end
 
 function unittests
 	test_fixClass;
+	test_doNotRemoveSingleton;
 end
 
 function test_fixClass
@@ -38,4 +43,17 @@ function test_fixClass
 		                 'data', [1 1; 2 2; 6 6; 6 7; 7 6; 7 7; 6.5 7; 10 10; 11 10; 10 11]);
 	new = fix_class_unlabeled_clusters(indiv, gmmObj, sharedData);
 	assertTrue(isequal(new.classOfCluster, [1 2 3 1 2]));
+end
+
+function test_doNotRemoveSingleton
+	%caso de soluções infeasible em que o objeto rotulado para um chunklet pode estar
+	%em um grupo de outra classe, deve-se tomar cuidado adicional para não remover o 
+	%único cluster daquela classe
+	indiv = struct('nClusters',5, 'classOfCluster', [ 1 2 3 2 1], ...
+		              'mean', [1 1; 6.9 6.9; 10.1 10.1; 2 2; 6 6 ]);
+	gmmObj = struct('clusterLabels', [ 1 4 5 2 2 2 3 3 3 ]);
+	sharedData = struct('chunklets', [ 1 0 3 2 2 2 0 0 0 ], ...
+		                 'data', [1 1; 2 2; 6 6; 6 7; 7 6; 7 7; 6.5 7; 10 10; 11 10; 10 11]);
+	new = fix_class_unlabeled_clusters(indiv, gmmObj, sharedData);
+	assertEqual(new.classOfCluster, [1 2 3 1 1]);
 end
